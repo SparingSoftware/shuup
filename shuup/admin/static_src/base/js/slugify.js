9fdd6694979489a150850402f5a223f8a39be76a
_bartek <barteksielicki@gmail.com>
2017-02-01 13:41:36 +0100

    modify function for slug creation

--- shuup/admin/static_src/base/js/slugify.js
+++ shuup/admin/static_src/base/js/slugify.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /**
  * This file is part of Shuup.
  *
@@ -7,16 +9,54 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-
-(function() {
+(function () {
     // https://gist.github.com/mathewbyrne/1280286
+    var specialChars = {
+        "ą": "a",
+        "ć": "c",
+        "ę": "e",
+        "ń": "n",
+        "ł": "l",
+        "ó": "o",
+        "ś": "s",
+        "ź": "z",
+        "ż": "z"
+    };
+
     function slugify(text) {
-      return text.toString().toLowerCase()
-        .replace(/\s+/g, '-')           // Replace spaces with -
-        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-        .replace(/^-+/, '')             // Trim - from start of text
-        .replace(/-+$/, '');            // Trim - from end of text
+        text = text.toString().toLowerCase().replace(/\s+/g, '-') // Replace spaces with -
+        .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+        .replace(/\-\-+/g, '-') // Replace multiple - with single -
+        .replace(/^-+/, '') // Trim - from start of text
+        .replace(/-+$/, ''); // Trim - from end of text
+
+        // Replace special characters
+        var _iteratorNormalCompletion = true;
+        var _didIteratorError = false;
+        var _iteratorError = undefined;
+
+        try {
+            for (var _iterator = specialChars[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                var c = _step.value;
+
+                text = text.replace(c, specialChars[c]);
+            }
+        } catch (err) {
+            _didIteratorError = true;
+            _iteratorError = err;
+        } finally {
+            try {
+                if (!_iteratorNormalCompletion && _iterator.return) {
+                    _iterator.return();
+                }
+            } finally {
+                if (_didIteratorError) {
+                    throw _iteratorError;
+                }
+            }
+        }
+
+        return text;
     }
 
     function autoSlugify() {
