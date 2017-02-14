392506f9e6984bb0c5603a6f5cdd6ddbe6e3a383
_bartek <barteksielicki@gmail.com>
2017-02-14 20:22:16 +0100

    remove optional/required label from hepl text

--- shuup/admin/utils/bs3_renderers.py
+++ shuup/admin/utils/bs3_renderers.py
@@ -62,10 +62,6 @@ class AdminFieldRenderer(FieldRenderer):
 
     def append_to_field(self, html):
         if self.field_help:
-            if self.field.field.required:
-                self.field_help = _("Required") + _("%s" % self.field_help)
-            else:
-                self.field_help = _("Optional") + _("%s" % self.field_help)
             html += "<span class='help-popover-btn'>"
             # tabindex is required for popover to function but we don't actually want to be able to tab to it
             # so set a large tabindex
